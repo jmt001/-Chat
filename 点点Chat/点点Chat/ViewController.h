aa3dfcbcb775f8cbf6fbc6737a6f21970fefb13c
xyfqldy <1399140589@qq.com>
2015-12-05 09:19:48 +0800

    点点APP12.5---9：19第一次上传

---
+++ 点点Chat/点点Chat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  点点Chat
+//
+//  Created by jiuxiaoming on 15/12/5.
+//  Copyright © 2015年 jiuxiaoming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 点点Chat/点点Chat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  点点Chat
+//
+//  Created by jiuxiaoming on 15/12/5.
+//  Copyright © 2015年 jiuxiaoming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
